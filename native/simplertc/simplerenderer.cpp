d36ef633ab7f2d62c250dbed1b03524bbd226340
Zhou Chang <achang.zhou@gmail.com>
2013-06-30 20:54:17 +0800

    Added remove video display Signed-off-by: Zhou Chang <achang.zhou@gmail.com>

--- native/simplertc/simplerenderer.cpp
+++ native/simplertc/simplerenderer.cpp
@@ -1,20 +1,92 @@
 #include <iostream>
 #include "simplerenderer.h"
+#include <gtk/gtk.h>
+#include <stddef.h>
 
 SimpleVideoRenderer::SimpleVideoRenderer() {
-    std::cout << "Created a SimpleVideoRenderer" << std::endl;
+    gui_thread_ = new talk_base::Thread();
+    gui_thread_->Start();
+    gui_thread_->Post(this, 0);
 }
 
 SimpleVideoRenderer::~SimpleVideoRenderer() {
 
 }
 
+void SimpleVideoRenderer::OnMessage(talk_base::Message *msg) {
+    gtk_init(NULL, NULL);
+    g_type_init();
+    g_thread_init(NULL);
+    window_ = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    if (window_) {
+        gtk_window_set_position(GTK_WINDOW(window_), GTK_WIN_POS_CENTER);
+        gtk_window_set_default_size(GTK_WINDOW(window_), 640, 480);
+        gtk_window_set_title(GTK_WINDOW(window_), "RemoteVideo");
+        
+        gtk_container_set_border_width(GTK_CONTAINER(window_), 0);
+        draw_area_ = gtk_drawing_area_new();
+        gtk_container_add(GTK_CONTAINER(window_), draw_area_);
+
+        gtk_widget_show_all(window_);
+    }  
+    gtk_main();
+       
+}
+
 void SimpleVideoRenderer::SetSize(int width, int height) {
-    std::cout << "SetSize width=" << width << " height=" << height;
+    gdk_threads_enter();
+    width_ = width;
+    height_ = height;
+    image_.reset(new uint8[width * height * 4]);
+    gdk_threads_leave();
+}
+
+gboolean Redraw(gpointer data) {
+    SimpleVideoRenderer* wnd = reinterpret_cast<SimpleVideoRenderer*>(data);
+    wnd->OnRedraw();
+    return false;
+}
+
+void SimpleVideoRenderer::OnRedraw() {
+    gdk_threads_enter();
+
+    gdk_draw_rgb_32_image(draw_area_->window,
+            draw_area_->style->fg_gc[GTK_STATE_NORMAL],
+            0,
+            0,
+            width_,
+            height_,
+            GDK_RGB_DITHER_MAX,
+            image_.get(),
+            width_ * 4 );
+
+    gdk_threads_leave();
 }
 
 void SimpleVideoRenderer::RenderFrame(const cricket::VideoFrame* frame) {
-    std::cout << " >>>>> RenderFrame >>>>> " << std::endl;
+    gdk_threads_enter();
+
+    int size = width_ * height_ * 4;
+    // TODO: Convert directly to RGBA
+    frame->ConvertToRgbBuffer(cricket::FOURCC_ARGB,
+            image_.get(),
+            size,
+            width_ * 4);
+    // Convert the B,G,R,A frame to R,G,B,A, which is accepted by GTK.
+    // The 'A' is just padding for GTK, so we can use it as temp.
+    uint8* pix = image_.get();
+    uint8* end = image_.get() + size;
+    while (pix < end) {
+        pix[3] = pix[0];     // Save B to A.
+        pix[0] = pix[2];  // Set Red.
+        pix[2] = pix[3];  // Set Blue.
+        pix[3] = 0xFF;     // Fixed Alpha.
+        pix += 4;
+    }
+
+    gdk_threads_leave();
+
+    g_idle_add(Redraw, this); 
 }
 
 
--- native/simplertc/simplerenderer.h
+++ native/simplertc/simplerenderer.h
@@ -7,14 +7,32 @@
 #include "talk/media/base/videoframe.h"
 #include "talk/media/base/videorenderer.h"
 
-class SimpleVideoRenderer : public webrtc::VideoRendererInterface {
+typedef struct _GtkWidget GtkWidget;
+typedef union _GdkEvent GdkEvent;
+
+class SimpleVideoRenderer : public webrtc::VideoRendererInterface, public talk_base::MessageHandler {
 public:
     SimpleVideoRenderer();
     virtual ~SimpleVideoRenderer();
     
-   // VideoRendererInterface implementation
+    void OnRedraw();
+
+    // MessageHandler
+    virtual void OnMessage(talk_base::Message *msg);
+
+    // VideoRendererInterface implementation
     virtual void SetSize(int width, int height);
     virtual void RenderFrame(const cricket::VideoFrame* frame);
+
+private:
+    GtkWidget* window_;  // Our main window.
+    GtkWidget* draw_area_;  // The drawing surface for rendering video streams. 
+
+    talk_base::Thread* gui_thread_;    
+    int width_;
+    int height_;
+    talk_base::scoped_array<uint8> image_;
 };
+
 
 #endif
