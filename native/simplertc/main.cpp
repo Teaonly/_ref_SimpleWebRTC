44b76d8d06d17dfcd0b158b637d877feef895fc6
Zhou Chang <achang.zhou@gmail.com>
2014-02-02 16:24:25 +0800

    Update to worable WebRTC Signed-off-by: Zhou Chang <achang.zhou@gmail.com>

--- native/simplertc/main.cpp
+++ native/simplertc/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <stdlib.h>
 
+#include "talk/base/ssladapter.h"
 #include "talk/base/basicdefs.h"
 #include "talk/base/common.h"
 #include "talk/base/helpers.h"
@@ -9,12 +10,14 @@
 #include "simplertc.h"
 
 int main(int argc, char *argv[]) {
+    talk_base::InitializeSSL(); 
+    
     //talk_base::LogMessage::LogToDebug(talk_base::LS_VERBOSE);
     //talk_base::LogMessage::LogToDebug(talk_base::LS_WARNING);
     talk_base::LogMessage::LogToDebug(talk_base::LS_ERROR);
     talk_base::LogMessage::LogTimestamps();
     talk_base::LogMessage::LogThreads();
-
+    
     if ( argc < 4) {
         std::cout << "usage: mixer server port name" << std::endl;
         return -1;
